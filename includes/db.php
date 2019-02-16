b798c716426d5dbdb6df7b8f1b9d33e47d6dcebd
Sazzad Bin Ashique <sazzad.sumon35@gmail.com>
2019-02-16 14:05:10 +0600

    cms development & fetch the data from database

---
+++ includes/db.php
@@ -0,0 +1,22 @@
+<?php 
+// phpinfo();
+
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_pass'] = "123456";
+$db['db_name'] = "luminous";
+
+foreach ($db as $key => $value) {
+	define(strtoupper($key), $value);
+}
+
+$connection =mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+ // if ($connection) {
+ // 	echo "We are connected";
+ // }
+
+
+
+
+?>
