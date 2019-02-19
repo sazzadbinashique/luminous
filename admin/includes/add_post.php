97203d00b8264cf1db759a31c49e93f7e29d2937
Sazzad Bin Ashique <sazzad.sumon35@gmail.com>
2019-02-19 13:10:14 +0600

    insert post create

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,49 +1,93 @@
+<?php 
+
+if (isset($_POST['create_post'])) {
+
+	$post_category_id= $_POST['post_category_id'];
+	$post_title= $_POST['post_title'];
+	$post_author= $_POST['post_author'];
+
+
+	$post_image= $_FILES['post_image']['name'];
+	$post_image_temp= $_FILES['post_image']['tmp_name'];
+
+	$post_tags= $_POST['post_tags'];
+	$post_content= $_POST['post_content'];
+	$post_date= date('d-m-y');
+	$post_comment_count= 4;
+	$post_status= $_POST['post_status'];
+
+
+ 
+
+	move_uploaded_file($post_image_temp, "../images/$post_image");
+
+
+
+
+	 $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count, post_status)";
+	 $query.= "VALUES ({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_comment_count}', '{$post_status}') ";
+
+
+	 $create_post_query = mysqli_query($connection, $query);
+
+	if (!$create_post_query) {
+
+        die('QUERY FAILED' . mysqli_error($connection));
+    }
+
+
+
+}
+
+
+
+
+ ?>
+
+
+
+
+
+
 <form action="" method="post" enctype="multipart/form-data">
 	
 	<div class="form-group">
 		<label for="">Post Title</label>
 		<input type="text" class="form-control" name="post_title">
 	</div>	
-
 	<div class="form-group">
 		<label for="">Post Category Id</label>
-		<input type="text" class="form-control" name="post_title">
+		<input type="text" class="form-control" name="post_category_id">
 	</div>	
-
 
 	<div class="form-group">
 		<label for="">Post Author</label>
-		<input type="text" class="form-control" name="post_title">
+		<input type="text" class="form-control" name="post_author">
 	</div>	
-
 	<div class="form-group">
 		<label for="">Post Status</label>
-		<input type="text" class="form-control" name="post_title">
+		<input type="text" class="form-control" name="post_status">
 	</div>	
-
 	<div class="form-group">
 		<label for="">Post Image</label>
-		<input type="file" class="form-control" name="post_title">
-	</div>	
+		<input type="file" class="form-control" name="post_image">
+	</div>
+	
 
 	<div class="form-group">
 		<label for="">Post Tags</label>
-		<input type="text" class="form-control" name="post_title">
+		<input type="text" class="form-control" name="post_tags">
 	</div>	
-
 	<div class="form-group">
 		<label for="">Post Content</label>
-		<input type="text" class="form-control" name="post_title">
+		<textarea  class="form-control" name="post_content" id="" cols="30" rows="10"></textarea>
 	</div>	
 	<div class="form-group">
 		<label for="">Post Date</label>
-		<input type="text" class="form-control" name="post_title">
+		<input type="date" class="form-control" name="post_date">
 	</div>	
 	<div class="form-group">
-		<label for="">Post Title</label>
-		<input type="text" class="form-control" name="post_title">
+		<input type="submit" class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
 	</div>	
-
-
 
 </form>
